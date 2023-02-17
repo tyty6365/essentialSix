1dffc499cacfdd67942e6309b1172a641de6dc72
Tyler Scott <tyscott2021@gmail.com>
2023-02-17 12:33:42 -0600

    new commit_2 02/17

---
+++ src/Welpage.java
@@ -0,0 +1,30 @@
+public class Welpage {
+
+        private int numID;
+
+
+        public Welpage(int numID) {
+
+
+
+            this.numID = numID;
+
+
+        }
+
+        public int getnumID() {
+
+            return numID;
+
+        }
+
+
+
+        public void setName(int numID) {
+
+            this.numID = numID;
+
+        }
+
+    }
+
